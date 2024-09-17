3642b50c3f0078a5cfb9ec31d204050f7a28d569
EDUARDO SANTOS DA SILVA <eduardo.ssilva36@senacsp.edu.br>
2024-09-16 22:20:37 -0300

    primeiro commit

---
+++ Model.py
@@ -0,0 +1,24 @@
+class Model:
+    def __init__(self):
+        #self.num1 = 0
+        #self.num2 = 0
+
+
+     """def trocar(self, valorA, valorB):
+        valorC = valorA
+        valorA = valorB
+        valorB = valorC
+        return f'Valor A {valorA} \nValor B: {valorB}'
+
+    def tabuada(self, num):
+        resultado = " " #Variavél String
+        for i in range(0, 11, 1): #passa parâmetros
+            resultado += f'{num} * {i} = {num * i}\n' # += resultado = resultado + resultado
+        return resultado
+
+    def mediaTres(self, nota1, nota2, nota3):
+        return (nota1 + nota2 + nota3)/3"""
+
+    def exercicioUm(self,num):
+        num = [0,1,2,3,4,5,6,7,8,9,10]
+        print(num[0,11])
---
+++ main.py
@@ -0,0 +1,5 @@
+from Control import Control #Vou chamar a pasta Control do arquivo Control
+
+if __name__ == '__main__':
+    controle = Control()
+    controle.operacoes()
